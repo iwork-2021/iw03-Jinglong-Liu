00886174e574295d66ad5eb7242ce1dbb41fd02e
Jinglong-Liu <1601029287@qq.com>
2021-11-07 10:05:39 +0800

    about us

--- ITSC/AboutUsViewController.swift
+++ ITSC/AboutUsViewController.swift
@@ -12,100 +12,66 @@ class AboutUsViewController: UIViewController {
 
     @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
-            loadHTMLFragment()
+            self.loadWeb()
             super.viewDidLoad()
             // Do any additional setup after loading the view.
         }
     
-    func loadHTMLFragment() {
-            let content = """
-                <div class="post post1 post-91 mbox" frag="窗口91">
-                                    <div class="con">
-                                        <div id="wp_news_w91">
-
-                                            <ul class="news_list clearfix">
-                                                
-                                                <li class="news n1 clearfix">
-                                                    <div class="news_box">
-                                                        <div class="news_title">
-                                                        
-                                                        服务电话</a>
-                                                        
-                                                        </div>
-                                                        <div class="news_fbt">025-89683791</div>
-                                                        <div class="news_text">服务时间：每天（8：00-19：00）</div>
-                                                    </div>
-                                                </li>
-                                                
-                                                <li class="news n2 clearfix">
-                                                    <div class="news_box">
-                                                        <div class="news_title">
-                                                        
-                                                        校园卡电话</a>
-                                                        
-                                                        </div>
-                                                        <div class="news_fbt">025-89683791</div>
-                                                        <div class="news_text">服务时间：每天（8：00-19：00）</div>
-                                                    </div>
-                                                </li>
-                                                
-                                                <li class="news n3 clearfix">
-                                                    <div class="news_box">
-                                                        <div class="news_title">
-                                                        
-                                                        服务邮箱</a>
-                                                        
-                                                        </div>
-                                                        <div class="news_fbt">ITSC@nju.edu.cn</div>
-                                                        <div class="news_text"></div>
-                                                    </div>
-                                                </li>
-                                                
-                                                <li class="news n4 clearfix">
-                                                    <div class="news_box">
-                                                        <div class="news_title">
-                                                        
-                                                        招聘邮箱</a>
-                                                        
-                                                        </div>
-                                                        <div class="news_fbt">ITSChr@nju.edu.cn</div>
-                                                        <div class="news_text"></div>
-                                                    </div>
-                                                </li>
-                                                
-                                                <li class="news n5 clearfix">
-                                                    <div class="news_box">
-                                                        <div class="news_title">
-                                                        
-                                                        仙林信息化中心楼</a>
-                                                        
-                                                        </div>
-                                                        <div class="news_fbt"></div>
-                                                        <div class="news_text">前台工作时间：每天8：00–19：00</div>
-                                                    </div>
-                                                </li>
-                                                
-                                                <li class="news n6 clearfix">
-                                                    <div class="news_box">
-                                                        <div class="news_title">
-                                                        
-                                                        鼓楼综合服务大厅</a>
-                                                        
-                                                        </div>
-                                                        <div class="news_fbt"></div>
-                                                        <div class="news_text">工作日：9:00–12:00、13:00–17:00</div>
-                                                    </div>
-                                                </li>
-                                                
-                                            </ul>
-                                        </div>
-
-                                    </div>
-                                </div>
-            """
-            
-            webView.loadHTMLString(content, baseURL: nil)
-        }
+    
+        private func loadWeb(){
+            print("about")
+            let url = URL(string: "https://itsc.nju.edu.cn/xwdt/list.htm")!
+            let task = URLSession.shared.dataTask(with: url, completionHandler: {
+                data, response, error in
+                if let error = error {
+                    print("\(error.localizedDescription)")
+                    return
+                }
+                guard let httpResponse = response as? HTTPURLResponse,
+                    (200...299).contains(httpResponse.statusCode) else {
+                    print("server error")
+                    return
+                }
+                
+                if let mimeType = httpResponse.mimeType, mimeType == "text/html",
+                            let data = data,
+                            let string = String(data: data, encoding: .utf8) {
+                                DispatchQueue.main.async {
+                                    let lines = string.split(separator: "\r\n")
+                                    var flag = false
+                                    var timetorecord = false
+                                    var content = "<html><body>"
+                                    for line in lines{
+                                        let symbol = line.split(separator: "\t")
+                                        if line == "<!--Start||footer-->"{
+                                            flag = true
+                                        }
+                                        else if line == "<!--End||footer-->"{
+                                            flag = false
+                                        }
+                                        if flag{
+                                           
+                                            
+                                            if !symbol.isEmpty && symbol[0] == "<div class=\"foot-center\">"{
+                                                timetorecord = true
+                                            }
+                                            if !symbol.isEmpty && symbol[0] == "<div class=\"foot-right\" >"{
+                                                timetorecord = false
+                                            }
+                                            if timetorecord && !symbol.isEmpty && symbol[0] != "<div class=\"foot-center\">"{
+                                                content += line
+                                            }
+                                        }
+                                    }
+                                    content += "</body></html>"
+                        self.webView.loadHTMLString(content, baseURL: nil)
+                }
+                    
+            }
+        
+        })
+            task.resume()
+    }
 
     /*
     // MARK: - Navigation
